b305edd2b37f1eb3b6a40ff480f2448616d50432
William PEPIN <williamp10022000@gmail.com>
2019-11-26 16:58:57 +0100

    Add player animator controller

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D body_;
+    Animator animator_;
+    bool isLookingRight_ = true;
+    SpriteRenderer spriteRenderer_;
+    
+    void Start()
+    {
+        animator_ = GetComponent<Animator>();
+        spriteRenderer_ = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    { 
+        animator_.SetFloat("speed", Mathf.Abs(body_.velocity.x));
+
+        if (body_.velocity.x < 0.1f && isLookingRight_)
+        {
+            spriteRenderer_.flipX = true;
+            isLookingRight_ = false;
+        }
+        else if (body_.velocity.x > 0.1f && !isLookingRight_)
+        {
+            spriteRenderer_.flipX = false;
+            isLookingRight_ = true;
+        }
+
+        animator_.SetBool("isJumping", false);
+    }
+}
