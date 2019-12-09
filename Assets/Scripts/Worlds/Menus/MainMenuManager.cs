812811fcd264626ecad3846545863d908e45c39f
William PEPIN <williamp10022000@gmail.com>
2019-12-09 15:45:53 +0100

    Add main menu

---
+++ Assets/Scripts/Worlds/Menus/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CollectibleObjectTestScene");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
